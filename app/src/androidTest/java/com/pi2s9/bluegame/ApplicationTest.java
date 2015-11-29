3c8638659f9fe80e1b247dcb1d864315d8bc3e2e
kelvin hado <kelvinhado@outlook.com>
2015-11-29 21:45:08 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/pi2s9/bluegame/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pi2s9.bluegame;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
